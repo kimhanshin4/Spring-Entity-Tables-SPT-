aa688a841740fde3836344f15604fab68ba37892
kimhanshin4 <funold1004@gmail.com>
2023-11-15 00:03:38 +0900

    @OneToMany 1대N 단방향 및 조회 테스트

--- src/test/java/com/sparta/jpaadvance/relation/ManyToOneTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/ManyToOneTest.java
@@ -29,12 +29,12 @@ public class ManyToOneTest {
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         Food food2 = new Food();
         food2.setName("양념 치킨");
         food2.setPrice(20000);
-        food2.setUser(user); // 외래 키(연관 관계) 설정
+//        food2.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -56,8 +56,8 @@ public class ManyToOneTest {
         // 외래 키의 주인이 아닌 User 에서 Food 를 저장해보겠습니다.
         User user = new User();
         user.setName("Robbie");
-        user.getFoodList().add(food);
-        user.getFoodList().add(food2);
+//        user.getFoodList().add(food);
+//        user.getFoodList().add(food2);
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -83,8 +83,8 @@ public class ManyToOneTest {
         // 해당 메서드에 외래 키(연관 관계) 설정 food.setUser(this); 추가
         User user = new User();
         user.setName("Robbie");
-        user.addFoodList(food);
-        user.addFoodList(food2);
+//        user.addFoodList(food);
+//        user.addFoodList(food2);
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -101,12 +101,12 @@ public class ManyToOneTest {
         Food food = new Food();
         food.setName("고구마 피자");
         food.setPrice(30000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         Food food2 = new Food();
         food2.setName("아보카도 피자");
         food2.setPrice(50000);
-        food2.setUser(user); // 외래 키(연관 관계) 설정
+//        food2.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -120,7 +120,7 @@ public class ManyToOneTest {
         System.out.println("food.getName() = " + food.getName());
 
         // 음식을 주문한 고객 정보 조회
-        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+//        System.out.println("food.getUser().getName() = " + food.getUser().getName());
     }
 
     @Test
@@ -131,10 +131,10 @@ public class ManyToOneTest {
         System.out.println("user.getName() = " + user.getName());
 
         // 해당 고객이 주문한 음식 정보 조회
-        List<Food> foodList = user.getFoodList();
-        for (Food food : foodList) {
-            System.out.println("food.getName() = " + food.getName());
-            System.out.println("food.getPrice() = " + food.getPrice());
-        }
+//        List<Food> foodList = user.getFoodList();
+//        for (Food food : foodList) {
+//            System.out.println("food.getName() = " + food.getName());
+//            System.out.println("food.getPrice() = " + food.getPrice());
+//        }
     }
 }
--- src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
+++ src/test/java/com/sparta/jpaadvance/relation/OneToOneTest.java
@@ -29,7 +29,7 @@ public class OneToOneTest {
         Food food = new Food();
         food.setName("후라이드 치킨");
         food.setPrice(15000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -82,7 +82,7 @@ public class OneToOneTest {
         Food food = new Food();
         food.setName("고구마 피자");
         food.setPrice(30000);
-        food.setUser(user); // 외래 키(연관 관계) 설정
+//        food.setUser(user); // 외래 키(연관 관계) 설정
 
         userRepository.save(user);
         foodRepository.save(food);
@@ -95,7 +95,7 @@ public class OneToOneTest {
         System.out.println("food.getName() = " + food.getName());
 
         // 음식을 주문한 고객 정보 조회
-        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+//        System.out.println("food.getUser().getName() = " + food.getUser().getName());
     }
 
     @Test
