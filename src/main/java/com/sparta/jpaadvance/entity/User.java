2a709ddc4b177a8fc4d7b8e7c52c71ac21e808f1
kimhanshin4 <funold1004@gmail.com>
2023-11-15 15:42:49 +0900

    CascadeType.PERSIST,OrphanRemoval = true

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -18,7 +18,8 @@ public class User {
     private String name;
     //FK의 주인 지정
 //    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST) //Default=LAZY, Cascade Persistence Option 적용
-    @OneToMany(mappedBy = "user",cascade = {CascadeType.PERSIST,CascadeType.REMOVE}) //Cascade Persistence Option 적용
+//    @OneToMany(mappedBy = "user",cascade = {CascadeType.PERSIST,CascadeType.REMOVE}) //Cascade Persistence Option 적용 => 연관 삭제 //옵션을 중복으로 넣을 시 {}
+    @OneToMany(mappedBy = "user",cascade = CascadeType.PERSIST,orphanRemoval = true)
     //DB에 직접적 저장x //관계를 표시하기 위한 하나의 방법
     private List<Food> foodList = new ArrayList<>();
 
