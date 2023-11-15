32af493d3d2bc20a851e527693f4f63f47093d1d
kimhanshin4 <funold1004@gmail.com>
2023-11-15 15:18:03 +0900

    영속성 전이 Cascade Persistence Option

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,7 +17,13 @@ public class User {
     private Long id;
     private String name;
     //FK의 주인 지정
-    @OneToMany(mappedBy = "user") //Default=LAZY
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST) //Default=LAZY, Cascade Persistence Option 적용
+    @OneToMany(mappedBy = "user",cascade = {CascadeType.PERSIST,CascadeType.REMOVE}) //Cascade Persistence Option 적용
     //DB에 직접적 저장x //관계를 표시하기 위한 하나의 방법
     private List<Food> foodList = new ArrayList<>();
+
+    public void addFoodList(Food food) {
+        this.foodList.add(food);
+        food.setUser(this); // FK 설정!
+    }
 }
